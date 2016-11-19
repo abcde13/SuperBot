cacccebedd83d89a50af49efaadcb0a720af22da
neji49 <neji49@gmail.com>
2016-11-19 04:02:32 -0500

    Updating final function call in game_state_update_event

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,7 @@ use discord::model::ServerId;
 use discord::model::VoiceState;
 use discord::model::Presence;
 use discord::model::RoleId;
+use discord::model::PublicChannel;
 use std::process::Command;
 use libc::*;
 use std::str;
@@ -189,22 +190,23 @@ fn dispatch_on_event(discord: &Discord, connection: &mut Connection) {
 
     match connection.recv_event() {
         
-        // Event when something happens on a voice channel
-        Ok(Event::VoiceStateUpdate(server_opt, voice_state)) => voice_channel_update_event(discord, connection, &server_opt, &voice_state, discord_info, &mut discord_state),
+        Ok(Event::VoiceStateUpdate(server_opt, voice_state)) => voice_channel_update_event(discord, connection, &server_opt, &voice_state, &discord_info, &mut discord_state),
 
-        // Presence includes change of game state for users
-        Ok(Event::PresenceUpdate{presence, server_id, roles}) => game_state_update_event(discord, connection, presence, &server_id, &roles, discord_info, &mut discord_state),
+        Ok(Event::PresenceUpdate{presence, server_id, roles}) => game_state_update_event(discord, connection, presence, &server_id, &roles, &discord_info, &mut discord_state),
+
         Ok(_) => {}
+        
         Err(discord::Error::Closed(code, body)) => {
             println!("Gateway closed on us with code {:?}: {}", code, body);
             process::exit(1);
         },
+
         Err(err) => println!("Receive error: {:?}", err)
     }
 }
 
 /// Event runs when an occurence happens on the voice channel.
-fn voice_channel_update_event(discord: &Discord, connection: &mut Connection, server_opt: &Option<ServerId>, voice_state: &VoiceState, info: DiscordInfo, state: &mut DiscordState) {
+fn voice_channel_update_event(discord: &Discord, connection: &mut Connection, server_opt: &Option<ServerId>, voice_state: &VoiceState, info: &DiscordInfo, state: &mut DiscordState) {
     println!("Got voice update: {:?},{:?}",server_opt,voice_state.channel_id);
 
     if server_opt.is_some() && voice_state.channel_id.is_some() {
@@ -214,21 +216,9 @@ fn voice_channel_update_event(discord: &Discord, connection: &mut Connection, se
         let channel: Channel = discord.get_channel(voice_state.channel_id.expect("No Channel")).unwrap();
 
         match channel {
+            Channel::Public(ref voice) if voice.kind == ChannelType::Voice => channel_is_voice(discord, connection, server, voice, voice_state, info, state),
 
-            Channel::Public(ref voice) if voice.kind == ChannelType::Voice => {
-
-                let name_and_channel_match = user.display_name() == info.username && voice.name == info.channel;
-
-                if  name_and_channel_match {
-                    *state.user_in_channel = !*state.user_in_channel;
-                    println!("user_in_channel {}", state.user_in_channel);
-                    println!("user_in_game {}", state.user_in_game);
-                    *state.channel_id = voice_state.channel_id;
-                }
-
-                check_state_and_join_channel(connection, server, state);
-            }
-            _ => println!("Not a voice channel")
+            _ => println!("Not a voice channel"),
         }
     } else {
         *state.user_in_channel = false;
@@ -236,7 +226,7 @@ fn voice_channel_update_event(discord: &Discord, connection: &mut Connection, se
 }
 
 /// Event dispatch for when state of game changes.
-fn game_state_update_event(discord: &Discord, connection: &mut Connection, presence: Presence, server_id: &Option<ServerId>, roles: &Option<Vec<RoleId>>, info: DiscordInfo, state: &mut DiscordState) {
+fn game_state_update_event(discord: &Discord, connection: &mut Connection, presence: Presence, server_id: &Option<ServerId>, roles: &Option<Vec<RoleId>>, info: &DiscordInfo, state: &mut DiscordState) {
     let user = discord.get_member(server_id.expect("No Server"), presence.user_id).unwrap();
     let server = server_id;
 
@@ -263,6 +253,23 @@ fn game_state_update_event(discord: &Discord, connection: &mut Connection, prese
     }
 }
 
+/// Update state channel id if voice channel.
+fn channel_is_voice(discord: &Discord, connection: &mut Connection, server: &Option<ServerId>, voice: &PublicChannel, voice_state: &VoiceState, info: &DiscordInfo, state: &mut DiscordState) {
+    let user = discord.get_member(server.expect("No Server"),voice_state.user_id).unwrap();
+    let channel: Channel = discord.get_channel(voice_state.channel_id.expect("No Channel")).unwrap();
+
+    let name_and_channel_match = user.display_name() == info.username && voice.name == info.channel;
+
+    if  name_and_channel_match {
+        *state.user_in_channel = !*state.user_in_channel;
+        println!("user_in_channel {}", state.user_in_channel);
+        println!("user_in_game {}", state.user_in_game);
+        *state.channel_id = voice_state.channel_id;
+    }
+
+    check_state_and_join_channel(connection, server, state);
+}
+
 /// Verifies desired state and has bot take action.
 fn check_state_and_join_channel(connection: &mut Connection, server: &Option<ServerId>, state: &mut DiscordState) {
     let in_game_in_channel_bot_not_in_channel = *state.user_in_game && *state.user_in_channel && !*state.bot_in_channel;
@@ -271,11 +278,13 @@ fn check_state_and_join_channel(connection: &mut Connection, server: &Option<Ser
 
         let voice = Some(connection.voice(*server));
         match *state.channel_id {
+
             Some(id) => {
                 println!("Joining");
                 voice.map(|v| v.connect(id));
                 *state.bot_in_channel = true;
             }
+
             None => println!("Never found channel id")
         }
     }
