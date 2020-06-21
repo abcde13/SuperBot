7b4f7e5780ac032c45f4bad8ffda38c0bf706d3f
Niraj Amalkanti <namalkanti@gmail.com>
2020-06-21 17:41:50 -0400

    Adding comments to dynamic_bot.rs

--- src/dynamic_bot.rs
+++ src/dynamic_bot.rs
@@ -7,6 +7,7 @@ use crate::internal_api::ApiResponse::{User, Continue, Logout};
 
 impl DynamicBot
 {
+    //Constructor returns LoggedOutDBot with registered user list
     pub fn new() -> LoggedOutDBot
     {
         let users = load_user_data("users.yml".to_string());
@@ -14,6 +15,7 @@ impl DynamicBot
         bot
     }
 
+    //Listens for users to login from api and returns music
     pub fn listen_respond_logout(self) -> LoggedOutDBot
     {
         loop
@@ -35,6 +37,7 @@ impl DynamicBot
 
 impl LoggedOutDBot
 {
+    //Login function returns usable dbot 
     pub fn login(self, name: String, channel: String, token: String) -> DynamicBot
     {
         let api = InternalApi::new(name, channel, token);
@@ -43,17 +46,20 @@ impl LoggedOutDBot
     }
 }
 
+//DynamicBot stores HashMap of users and their music and api
 pub struct DynamicBot
 {
     registered_users: HashMap<String, String>,
     api: InternalApi,
 }
 
+//LoggedOutDBot only stores HashMap of users.
 pub struct LoggedOutDBot
 {
     registered_users: HashMap<String, String>,
 }
 
+//Private function accesses internal hashmap
 impl DynamicBot
 {
     fn get_music(&self, user: String) -> String
@@ -66,12 +72,14 @@ impl DynamicBot
     }
 }
 
+//Users struct to load data from yml file
 #[derive(Serialize, Deserialize)]
 struct Users
 {
     users: HashMap<String, String>,
 }
 
+//Loads data from yaml file
 fn load_user_data(file_path: String) -> HashMap<String, String> 
 {
     let yaml_string: String= fs::read_to_string(file_path).expect("Couldn't open file");
